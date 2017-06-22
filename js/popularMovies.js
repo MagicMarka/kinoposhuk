4ee808045a70355e06b40644c964f932ba3fa950
MagicMarka <magicformarka@gmail.com>
2017-06-22 20:04:56 +0300

    img height fixed

--- js/popularMovies.js
+++ js/popularMovies.js
@@ -1,23 +1,25 @@
 $(document).ready(function () {
 
 var popMovies = [];
+var url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=436fedae44d7b81338702a9341ae7a74';
+var start = 0;
+var end = 4;
 var showMovies = function (popMovies, start, end) {
 	popMovies = popMovies.slice(start, end);
 	return popMovies;
 	console.log(popMovies);
-	getPopularMovies();
 };
-
-		function getPopularMovies() {
-			axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=436fedae44d7b81338702a9341ae7a74').then(function (response) {
-				console.log(response);
-				var popMovies = response.data.results;
 				var popOutput = '';
-				$.each(showMovies(popMovies, 0, 4), function (index, movie) {
+		function getPopularMovies() {
+			axios.get(url).then(function (response) {
+				console.log(response);
+				popMovies = response.data.results;
+				// var popOutput = '';
+				$.each(showMovies(popMovies, start, end), function (index, movie) {
 					popOutput += `
 					<div class="col-xs-12 col-sm-6 col-md-3">
 						<div class="film-block text-center">
-							<img class="film-block-img" src="https://image.tmdb.org/t/p/w300${movie.poster_path}">
+							<img class="film-block-img"src="https://image.tmdb.org/t/p/w300${movie.poster_path}">
 							<p class="film-block-title">${movie.title}</p>
 						</div>
 					</div>
@@ -26,17 +28,33 @@ var showMovies = function (popMovies, start, end) {
 			$('#popular').html(popOutput);
 			}).catch(function (err) {
 				console.log(err);
-				}); 
+				});
 		};
-
-
-
 getPopularMovies();
 
-// $('#more_popular').click( function(){ 
-// 	showMovie(popMovies, 5, 9);
-// }
-// );
+function morePopularMovies(start, end) {
+			axios.get(url).then(function (response) {
+				var movies = response.data.results;
+				// var moreOutput = '';
+				$.each(showMovies(popMovies, start, end), function (index, movie) {
+					popOutput += `
+					<div class="col-xs-12 col-sm-6 col-md-3">
+						<div class="film-block text-center">
+							<img class="film-block-img"src="https://image.tmdb.org/t/p/w300${movie.poster_path}">
+							<p class="film-block-title">${movie.title}</p>
+						</div>
+					</div>
+					`;});
+				});
+			$('#popular').html(popOutput);
+		}
+
+
+
+$('#more_popular').click( function(){
+start+=4;
+end+=4;
+morePopularMovies(start,end);
 
 });
 
@@ -72,4 +90,4 @@ getPopularMovies();
 //             $('#popular').html(popOutput);
 //         };
 // getPopularMovies();
-// });
+});

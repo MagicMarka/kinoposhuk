00c39661e223295029ffa619c166d44132959cfd
MagicMarka <magicformarka@gmail.com>
2017-06-23 11:37:27 +0300

    new movies, upcoming movies added

--- js/newMovies.js
+++ js/newMovies.js
@@ -1,32 +1,8 @@
 $(document).ready(function () {
-<<<<<<< HEAD
-  // var newMovies = [],
-  // // npageNumber = 1,
-  // newUrl = '',
-  // startNew = 0,
-  // endNew = 12,
-  // showNewMovies = function (start, end) {
-  //   newMovies = newMovies.slice(start, end);
-  //   return newMovies;
-  //   console.log(newMovies);
-  // },
-  
-=======
->>>>>>> master
 
 function getNewMovies() {
   axios.get('https://api.themoviedb.org/3/discover/movie?api_key=436fedae44d7b81338702a9341ae7a74&primary_release_date.gte=2017-06-01&sort_by=popularity.desc').then(function (response) {
   console.log(response);
-<<<<<<< HEAD
-  newMovies = response.data.results;
-  newOutput = '';
-  $.each(newMovies.slice(0,6), function (index, movie) {
-    newOutput += `
-      <div class="col-xs-12 col-sm-6 col-md-4 no-padding">
-        <div class="film-block-new text-center">
-          <img class="film-block-new-img" src="https://image.tmdb.org/t/p/w154${movie.poster_path}">
-          <p class="film-block-title">${movie.title}</p>
-=======
   var newMovies = response.data.results;
   var newOutput = '';
   $.each(newMovies.slice(0,6), function (index, movie) {
@@ -35,7 +11,6 @@ function getNewMovies() {
         <div class="film-block-new text-center">
           <img class="film-block-new-img" src="https://image.tmdb.org/t/p/w154${movie.poster_path}">
           <p class="film-block-new-title">${movie.title}</p>
->>>>>>> master
         </div>
       </div>
           `;
@@ -48,37 +23,4 @@ function getNewMovies() {
 
 getNewMovies();
 
-<<<<<<< HEAD
-// function moreNewMovies(startNew, endNew) {
-//   axios.get(url).then(function (response) {
-//   var movies = response.data.results;
-//   $.each(showNewMovies(newMovies, startNew, endNew), function (index, movie) {
-//     popOutput += `
-//       <div class="col-xs-12 col-sm-6 col-md-3 no-padding">
-//         <div class="film-block text-center">
-//           <img class="film-block-img"src="https://image.tmdb.org/t/p/w300${movie.poster_path}">
-//           <p class="film-block-title">${movie.title}</p>
-//         </div>
-//       </div>
-//       `;});
-//         });
-//   $('#newMovies').html(popOutput);
-// }
-
-// $('#more_new').click( function() {
-//   if(end > 20) {
-//     pageNumber +=1;
-//     start = 0;
-//     end = 3;
-//     console.log(url);
-//   }
-//   else {
-//     start += 3;
-//     end += 3;
-//   }
-// morePopularMovies(start,end);
-// });
-
-=======
->>>>>>> master
 });

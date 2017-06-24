7e812d6ea247f40d6cb9865e4a70385103059c08
MagicMarka <magicformarka@gmail.com>
2017-06-24 13:51:18 +0300

    com

--- js/getMovie.js
+++ js/getMovie.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 var movieID = '',
 	genres = [],
-	imdbId = '',
 	origTitle = '',
 	overview = '',
 	poster = '',
@@ -13,19 +12,22 @@ $('#popular').on('click', '.movie', function(){
 	movieID = $(this).data('id');
 	console.log(movieID);
 	movieInfo(movieID);
+	castsInfo(movieID);
 });
 
 function movieInfo(movieID) {
 	axios.get('https://api.themoviedb.org/3/movie/' + movieID + '?api_key=436fedae44d7b81338702a9341ae7a74').then(function (response) {
 		var movie = response.data;
 		genres = movie.genres;
-		origTitle = movie.title;
 		poster = 'https://image.tmdb.org/t/p/w300' + movie.poster_path;
 	})
 }
 function castsInfo(movieID) {
 	axios.get('https://api.themoviedb.org/3/movie/' + movieID + 'credits?api_key=436fedae44d7b81338702a9341ae7a74').then(function (response) {
-		movie = response.results;
+		var movie = response.data;
+		origTitle = movie.original_title;
+		overview = movie.overview;
+
 		console.log(movie);
 	});
 }
